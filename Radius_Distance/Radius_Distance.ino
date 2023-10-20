2b0826c59763a3bbbdaa4ae8e1963410eb08c3d6
Suren Bono <surenbono76@gmail.com>
2023-10-20 11:19:16 +0800

    Update Radius_Distance.ino

--- Radius_Distance/Radius_Distance.ino
+++ Radius_Distance/Radius_Distance.ino
@@ -1,6 +1,4 @@
 
-
-
 // Constants for the center point (in decimal degrees)
 const double centerLat = 3.0301;
 const double centerLong = 103.3185;
@@ -15,41 +13,41 @@ double degToRad(double degrees) {
 
   // Function to calculate the distance between two points using the Haversine formula
   double calculateDistance(double lat1, double long1, double lat2, double long2) {
-    // Convert latitude and longitude from degrees to radians
-      lat1 = degToRad(lat1);
-        long1 = degToRad(long1);
-          lat2 = degToRad(lat2);
-            long2 = degToRad(long2);
+  // Convert latitude and longitude from degrees to radians
+   lat1 = degToRad(lat1);
+   long1 = degToRad(long1);
+   lat2 = degToRad(lat2);
+   long2 = degToRad(long2);
 
-              // Haversine formula
-                double dLat = lat2 - lat1;
-                  double dLong = long2 - long1;
-                    double a = sin(dLat / 2) * sin(dLat / 2) + cos(lat1) * cos(lat2) * sin(dLong / 2) * sin(dLong / 2);
-                      double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-                        double distanceKm = 6371 * c; // Earth's radius in km
+   // Haversine formula
+   double dLat = lat2 - lat1;
+   double dLong = long2 - long1;
+   double a = sin(dLat / 2) * sin(dLat / 2) + cos(lat1) * cos(lat2) * sin(dLong / 2) * sin(dLong / 2);
+   double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+   double distanceKm = 6371 * c; // Earth's radius in km
 
-                          return distanceKm;
-                          }
+   return distanceKm;
+    }
 
-                          void setup() {
-                            Serial .begin(9600);
-                            // Example point (latitude, longitude) to check
-                              double pointLat = 3.021624;
-                                double pointLong =103.302568;
+   void setup() {
+   Serial.begin(9600);
+   // Example point (latitude, longitude) to check
+   double pointLat = 3.021624;
+   double pointLong =103.302568;
 
-                                  double distance = calculateDistance(centerLat, centerLong, pointLat, pointLong);
+   double distance = calculateDistance(centerLat, centerLong, pointLat, pointLong);
 
-                                    if (distance <= radiusKm) {
-                                        Serial.print("inside Kluang <30km, ");
-                                        Serial.print(distance);
-                                        Serial.print(" km");
-                                              }else{Serial.print("outside Kluang >30km, ");
-                                             Serial.print(distance);
-                                             Serial.print("km");
-                                                                                      }
-                                              }
+     if (distance <= radiusKm) {
+     Serial.print("inside Kluang <30km, ");
+     Serial.print(distance);
+     Serial.print(" km");
+     }else{Serial.print("outside Kluang >30km, ");
+     Serial.print(distance);
+     Serial.print("km");                                                                       }
+      }
 
-                                              void loop() {
-                                                // Your Arduino loop code
-                                                }
+       void loop() {
+              // Your Arduino loop code
+                    }
+//end of code
                                                 
